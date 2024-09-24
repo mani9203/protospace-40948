2e158a7a50e04512c2365e9f100fedd1980636cf
mani9203 <mana.june27.8807@outlook.jp>
2024-09-24 14:09:59 +0900

    削除機能修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -40,7 +40,6 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
-    @prototype = Prototype.find(params[:id])
     if @prototype.destroy
       redirect_to root_path
     else
